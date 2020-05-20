3d022b1a95cfd241f1f2a03fdf13fe7b6122d9f6
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-05-21 00:01:33 +0100

    first commit

---
+++ consumer/src/test/java/com/artemas/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemas.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
